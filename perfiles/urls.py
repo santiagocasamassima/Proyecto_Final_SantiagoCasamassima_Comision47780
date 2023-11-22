f622397a1fbecc0ac3f204bbdaab6ede56e2ff2f
Santiago Casamassima <scasamassima@gmail.com>
2023-11-21 22:14:05 -0300

    Registro de Usuarios

---
+++ perfiles/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+
+from perfiles.views import registro
+
+urlpatterns = [
+    path('registro/', registro, name="registro"),
+]
--- perfiles/views.py
+++ perfiles/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import login, authenticate
+from perfiles.forms import UserRegisterForm
 
-# Create your views here.
+
+
+def registro(request):
+    if request.method == "POST":
+        formulario = UserRegisterForm(request.POST)
+
+        if formulario.is_valid():
+            formulario.save()
+            url_exitosa = reverse("inicio")
+            return redirect(url_exitosa)
+    else:
+        formulario = UserRegisterForm()
+    return render(
+        request = request,
+        template_name="perfiles/registro.html",
+        context={"form": formulario},
+    )
